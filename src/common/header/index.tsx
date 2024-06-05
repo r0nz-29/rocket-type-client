04ceea70b0e86bd7572beea798bacd2b7f0d8896
Raunit S <raunits29@gmail.com>
2024-06-05 21:09:25 +0530

    rollback darkmode

--- src/common/header/index.tsx
+++ src/common/header/index.tsx
@@ -1,21 +1,21 @@
-import useDarkMode from "../../hooks/useDarkMode.ts";
-import {MdOutlineLightMode} from "react-icons/md";
-import {WiMoonAltWaxingCrescent4} from "react-icons/wi";
+// import useDarkMode from "../../hooks/useDarkMode.ts";
+// import {MdOutlineLightMode} from "react-icons/md";
+// import {WiMoonAltWaxingCrescent4} from "react-icons/wi";
 import {Link} from "react-router-dom";
 
 export function Header() {
-  const {darkMode, toggleTheme} = useDarkMode();
+  // const {darkMode, toggleTheme} = useDarkMode();
   return (
     <div className="container max-w-7xl py-4 text-nord2">
       <div className="flex justify-between items-center">
         <Link to="/" className="text-2xl font-bold heading-font dark:text-nord6">Rocket Type</Link>
-        <button
-          aria-label="toggle theme"
-          onClick={toggleTheme}
-          className="dark:text-nord6"
-        >
-          {darkMode ? <MdOutlineLightMode size={32}/> : <WiMoonAltWaxingCrescent4 size={32}/>}
-        </button>
+        {/*<button*/}
+        {/*  aria-label="toggle theme"*/}
+        {/*  onClick={toggleTheme}*/}
+        {/*  className="dark:text-nord6"*/}
+        {/*>*/}
+        {/*  {darkMode ? <MdOutlineLightMode size={32}/> : <WiMoonAltWaxingCrescent4 size={32}/>}*/}
+        {/*</button>*/}
       </div>
     </div>
   );
